5c3fe020c782ae6aad0b11f24643b5fc4d332392
Gonzalo Casas <casas@arch.ethz.ch>
2017-10-11 15:00:32 +0200

    Fix storage of aggregated data

--- aws-storage.js
+++ aws-storage.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk')
 
+const switches = [0, 1, 2, 3];
 const buildQueryParams = (devID) => ({
     TableName: 'devices',
     Key: {
@@ -9,58 +10,71 @@ const buildQueryParams = (devID) => ({
 
 const buildMessagesParams = (devID, payload) => {
     let fields = payload['payload_fields']
+    let newItem = {
+        message_id: `${devID}-${Date.now()}`,
+        device_name: devID,
+        counter: payload['counter'],
+        status: fields['status'],
+        battery: fields['battery'],
+        battery_percentage: fields['battery_percentage']
+    };
 
-    return [{
-        PutRequest: {
-            Item: {
-                message_id: `${devID}-${Date.now()}`,
-                device_name: devID,
-                counter: payload['counter'],
-                status: fields['status'],
-                switch0: fields['switch0'],
-                switch1: fields['switch1'],
-                switch2: fields['switch2'],
-                switch3: fields['switch3'],
-                battery: fields['battery'],
-                battery_percentage: fields['battery_percentage']
-            }
-        }
-    }]
+    for (let i of switches) {
+        let key = 'switch' + i
+        let value = fields[key]
+        newItem[key] = value
+    }
+
+    return [{ PutRequest: { Item: newItem } }]
 }
 
 const buildNewDeviceParams = (devID, payload) => {
     let fields = payload['payload_fields']
+    let newItem = {
+        device_name: devID,
+        first_seen: Date.now(),
+        first_battery_value: fields['battery'],
+        hardware_serial: payload['hardware_serial']
+    }
 
-    return [{
-        PutRequest: {
-            Item: {
-                device_name: devID,
-                first_seen: Date.now(),
-                first_battery_value: fields['battery'],
-                hardware_serial: payload['hardware_serial'],
-                switch0: fields['switch0'],
-                switch1: fields['switch1'],
-                switch2: fields['switch2'],
-                switch3: fields['switch3']
-            }
-        }
-    }]
+    for (let i of switches) {
+        let key = 'switch' + i
+        let keyLast = 'last_switch' + i
+        let value = fields[key]
+
+        newItem[key] = value
+        newItem[keyLast] = value
+    }
+
+    return [{ PutRequest: { Item: newItem } }]
 }
 
 const buildUpdateDeviceParams = (devID, payload, existingItem) => {
     let fields = payload['payload_fields']
 
-    for (let i of[0, 1, 2, 3]) {
+    for (let i of switches) {
         let key = 'switch' + i
+        let keyLast = 'last_switch' + i
         let value = fields[key]
 
-        // Status=0 means the counters have accummulated values from before
-        // and status=1 means the counters have been reset since the last tx
-        if (fields['status'] == 0) {
-            value -= existingItem[key]
+        // status=1 means first transmission since power up
+        // If status=0 and there's a previous key, we have two possibilities
+        if (fields['status'] == 0 && keyLast in existingItem) {
+            if (value > existingItem[keyLast]) {
+                // The value is greater than the last one, so, we need to add
+                // the difference between last and current
+                existingItem[key] += (value - existingItem[keyLast])
+            } else if (value < existingItem[keyLast]) {
+                // Or the value is smaller than the last one, because the
+                // counter rolled over 255. We need to add 255-last + current.
+                existingItem[key] += (255 - existingItem[keyLast] + value)
+            }
+        } else {
+            existingItem[key] += value
         }
 
-        existingItem[key] += value
+        // Then store the unmodified payload value as `last seen`
+        existingItem[keyLast] = fields[key]
     }
 
     return [{
@@ -79,8 +93,6 @@ const store = async(devID, payload) => {
     let documentClient = new AWS.DynamoDB.DocumentClient({
         region: process.env.AWS_REGION
     });
-
-    let writeParams = 1;
 
     try {
         let data = await documentClient.get(buildQueryParams(devID)).promise()
