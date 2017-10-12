469a0b9c58383ca7509ff64980a7422fb54341ec
Gonzalo Casas <casas@arch.ethz.ch>
2017-10-13 00:46:32 +0200

    Linter fixes

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ const moment = require('moment')
 
 // Minimal service metrics
 let dataCollectorStartTime = Date.now()
-let uplinksReceived = 0;
+let uplinksReceived = 0
 
 // Configure uplink handler
 const uplinkStorageHandler = (devID, payload) => {
-    uplinksReceived++;
-    console.log(`Received uplink from device '${devID}'`)
-    console.log(`Storing payload:`)
-    console.log(payload)
-    AWS.store(devID, payload)
+  uplinksReceived++
+  console.log(`Received uplink from device '${devID}'`)
+  console.log(`Storing payload:`)
+  console.log(payload)
+  AWS.store(devID, payload)
 }
 
 // Kick off TTN MQTT client
 TTN(uplinkStorageHandler)
-    .catch((err) => {
-        console.error(err)
-        process.exit(1)
-    });
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
 
 // Start serving metrics page
-module.exports = () => ({ 
-    uplinksReceived,
-    dataCollectorStartTime,
-    dataCollectorStartTimeFormatted: moment(dataCollectorStartTime).fromNow()
+module.exports = () => ({
+  uplinksReceived,
+  dataCollectorStartTime,
+  dataCollectorStartTimeFormatted: moment(dataCollectorStartTime).fromNow()
 })
